05f59d668a4a593fbec2cb6a34bfdb332f5b238f
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-01-13 19:47:05 +0900

    (2023.1.13) 참조 연산자

--- Application.cpp
+++ Application.cpp
@@ -1,81 +1,83 @@
 #include <iostream> // c++ 입출력 헤더 <iostream> 
-#include <math.h> // 수학 관련 라이브러리
 
-char alphabet = 'B';
-
-struct Player
+// (c++) & 참조 연산자
+/*
+void PointerFunction(int * x)
 {
-    int x;
-    int y;
-};
+    std::cout << "PointerFunction x의 주소 : " << &x << std::endl;
+    *x = 10000;
 
-struct Monster
+}
+
+void ReferenceFunction(int & x)
 {
-    int x;
-    int y;
-};
+    std::cout << "ReferenceFunction x의 주소 : " << &x << std::endl;
+    x = 1000;
+}
+*/
+
+// 디폴트 매개변수는 오른쪽에서부터 정의해야 합니다.
+void Calculator(int x, int y = 20, int z = 100)
+{
+    std::cout << "x의 값 : " << x << std::endl;
+    std::cout << "y의 값 : " << y << std::endl;
+    std::cout << "z의 값 : " << z << std::endl;
+}
 
 int main()
 {
-    // c++ 입출력 
+    // (c++) & 참조 연산자
     /*
-    bool condition = true; // 1 byte 메모리
+    // 포인터의 이름이나 주소 앞에 사용하여
+    // 포인터에 가리키는 주소에 저장된 값을 반환하는 연산자입니다.
 
-    char blood = 'A';
-    int value = 100;
-    float pi = 3.14;
+    // int value = 100;
+    // int value2 = 200;
+
+    // int & ref = value;
+
+    // 참조 연산자는 선언과 동시에 초기화가 되어야 합니다.
+    // ex) int & ref1;
+
+    // std::cout << &ref << std::endl;
+    // std::cout << &value << std::endl;
+
+    // 한번 초기화가 되면 다른 변수를 참조할 수 없습니다.
+    // ref = value2;
+
+    int data = 10;
+
+    PointerFunction(&data);
+    std::cout << "data의 값 : " << data << std::endl;
     
-    // C++에서 변수를 입력받는 방법
-    // >> (추출 연산자)
-    // 버퍼 안에 있는 내용을 저장시킬 수 있는
-    // 연산자입니다.
-    // std::cin >> value;
-
-    if (condition)
-    {
-        // << (삽입 연산자)
-        // 입력된 데이터는 출력 장치로 전달하며,
-        // "<<" 연산자를 사용하여 자신이 참조한 값을
-        // 반환하는 연산자입니다.
-
-        std::cout << "안녕하세요." << std::endl;
-        std::cout << blood << std::endl;
-        std::cout << value << std::endl;
-        std::cout << pi << std::endl;
-    }
+    ReferenceFunction(data);
+    std::cout << "data의 값 : " << data << std::endl;
     */
 
-    // 범위 지정 연산자
+    // 기본 매개변수
     /*
-    // 여러 범위에서 사용되는 식별자를 구분하는데
-    // 사용하는 연산자입니다.
-    char alphabet = 'A';
-    
-    std::cout << "지역 변수 alphabet의 값 : " << alphabet << std::endl;
-    std::cout << "전역 변수 alphabet의 값 : " << ::alphabet << std::endl;
+    // 함수를 선언 및 정의할 때 사용자가 미리 기본값을 지정하는 매개변수입니다.
+    int variable = 300;
+
+    // x = 100 <- [100]
+    // Calculator(variable) <- [300]
+    // x <- variable, y <- 10
+    Calculator(variable);
     */
 
-    // 문자열
-    /*
-    std::string name = "Leesin";
+    // 자료형 추론
+    // 자료형
 
-    std::cout << "name의 값 : " << name << std::endl;
-    */
+    // 들어오는 값에 따라 자동으로 자료형으로 추론해주는 자료형입니다.
+    auto signal = 100;
+    auto character = 'A';
+    auto decimal = 36.5;
 
-    Player player;
-    Monster monster = {5, 5};
+    std::cout << sizeof(character) << std::endl;
+    std::cout << sizeof(signal) << std::endl;
+    std::cout << sizeof(decimal) << std::endl;
 
-    player.x = 0;
-    player.y = 0;
 
-    int tempX = player.x - monster.x;
-    int tempY = player.y - monster.y;
-
-    // 루트
-    std::cout << sqrt(pow(tempX,2) + pow(tempY,2)) << std::endl;
-
-    // 거듭제곱
-    // std::cout << pow(10, 2) << std::endl;
-
+    return 0;
 }
 
