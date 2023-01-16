8f857847a1844339148cb53c2fd63c3b55f085e6
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-01-16 19:47:12 +0900

    (2023.1.16) 범위 기반 반복문

--- Application.cpp
+++ Application.cpp
@@ -1,81 +1,46 @@
 #include <iostream> // c++ 입출력 헤더 <iostream> 
 
-// (c++) & 참조 연산자
-/*
-void PointerFunction(int * x)
-{
-    std::cout << "PointerFunction x의 주소 : " << &x << std::endl;
-    *x = 10000;
-
-}
-
-void ReferenceFunction(int & x)
-{
-    std::cout << "ReferenceFunction x의 주소 : " << &x << std::endl;
-    x = 1000;
-}
-*/
-
-// 디폴트 매개변수는 오른쪽에서부터 정의해야 합니다.
-void Calculator(int x, int y = 20, int z = 100)
-{
-    std::cout << "x의 값 : " << x << std::endl;
-    std::cout << "y의 값 : " << y << std::endl;
-    std::cout << "z의 값 : " << z << std::endl;
-}
-
 int main()
 {
-    // (c++) & 참조 연산자
+    // 범위 기반 반복문
     /*
-    // 포인터의 이름이나 주소 앞에 사용하여
-    // 포인터에 가리키는 주소에 저장된 값을 반환하는 연산자입니다.
+    // 시작과 끝을 알려주지 않아도 처음부터 끝까지
+    // 순회하는 반복문입니다.
 
-    // int value = 100;
-    // int value2 = 200;
+    int data[5] = { 10,20,30,40,50 };
 
-    // int & ref = value;
+    for (const auto & x : data)
+    {
+        // 범위 기반 반복문은 인덱스에 접근이 불가능합니다.
+        std::cout << x << std::endl;
+    }
 
-    // 참조 연산자는 선언과 동시에 초기화가 되어야 합니다.
-    // ex) int & ref1;
-
-    // std::cout << &ref << std::endl;
-    // std::cout << &value << std::endl;
-
-    // 한번 초기화가 되면 다른 변수를 참조할 수 없습니다.
-    // ref = value2;
-
-    int data = 10;
-
-    PointerFunction(&data);
-    std::cout << "data의 값 : " << data << std::endl;
-    
-    ReferenceFunction(data);
-    std::cout << "data의 값 : " << data << std::endl;
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << data[i] << std::endl;
+    }
     */
 
-    // 기본 매개변수
-    /*
-    // 함수를 선언 및 정의할 때 사용자가 미리 기본값을 지정하는 매개변수입니다.
-    int variable = 300;
+    // 동적 할당
+    // new(메모리 할당), delete(메모리 해제)
+    int * ptr = new int;
 
-    // x = 100 <- [100]
-    // Calculator(variable) <- [300]
-    // x <- variable, y <- 10
-    Calculator(variable);
-    */
+    *ptr = 100;
 
-    // 자료형 추론
-    // 자료형
+    std::cout << "ptr이 가리키는 값 : " << *ptr << std::endl;
+    std::cout << "ptr의 값 : " << ptr << std::endl;
 
-    // 들어오는 값에 따라 자동으로 자료형으로 추론해주는 자료형입니다.
-    auto signal = 100;
-    auto character = 'A';
-    auto decimal = 36.5;
+    // 동적 메모리 할당 해제
+    delete ptr;
 
-    std::cout << sizeof(character) << std::endl;
-    std::cout << sizeof(signal) << std::endl;
-    std::cout << sizeof(decimal) << std::endl;
+    ptr = nullptr;
+
+    std::cout << "ptr이 가리키는 값 : " << *ptr << std::endl;
+    std::cout << "ptr의 값 : " << ptr << std::endl;
+  
+
+    // malloc         , free
+
 
 
     return 0;
