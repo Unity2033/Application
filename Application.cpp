8a8f53983eae40b673b087d39782e92aeb4e4c40
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-01-25 19:50:14 +0900

    (2023.1.25) 얕은 복사와 깊은 복사

--- Application.cpp
+++ Application.cpp
@@ -1,35 +1,66 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define BUFFER_SIZE 10000
 #include <iostream> // c++ 입출력 헤더 <iostream> 
 
 using namespace std;
 
-void Function(const char * textName)
+class IPhone
 {
-    FILE* file = fopen(textName, "r");
+public :
+    int * version;
 
-    char buffer[BUFFER_SIZE] = { 0, };
+    IPhone()
+    {
+        version = new int;
+        cout << "기본 생성자 호출" << endl;
+    }
 
-    fread(buffer, 1, BUFFER_SIZE, file);
-    printf("%s", buffer);
+    // 복사 생성자
+    IPhone(const IPhone & copyIPhone)
+    {
+        // 깊은 복사란?
+        // 객체를 복사할 때, 참조 값이 아닌 인스턴스 자체를 새로 
+        // 복사하여 서로 다른 메모리를 생성하는 복사입니다.
+        version = new int;
+        *version = *copyIPhone.version;
 
-    fclose(file);
+        cout << "복사 생성자 호출" << endl;
+        cout << "version : " << *version << endl;
+    }
 
-}
+    ~IPhone()
+    {
+        delete version;
+    }
+};
 
 int main()
 {
-    // 파일 입출력
-    // fopen(파일의 이름, 파일 모드)
-    // w = write <- 쓰기
-    // r = read <- 읽기 
-    
-    // FILE * file = fopen("data.txt", "w");
-    // fputs("Game Password", file);
-    // fclose(file);
+    // 복사 생성자란?
+    /*
+    // 같은 객체를 복사하여 생성시킬 때 호출되는 생성자입니다.  
+    IPhone se1;
 
-    // 파일 불러오기 
-    Function("Dragon.txt");
+    se1.price = 100000;
+    se1.size = 5;
+
+    // se2(se1)
+    IPhone se2 = se1;
+    */
+
+    // 얕은 복사
+    // 객체를 복사할 때 주솟값을 복사하여 같은 메모리를 가리키는
+    // 복사입니다.
+
+    IPhone se1;
+
+    *se1.version = 10;
+
+    // se2(se1)
+    IPhone se2 = se1;
+
+    *se2.version = 20;
+
+    cout << "*se1.version의 값 : " << *se1.version << endl;
+    cout << "*se2.version의 값 : " << *se2.version << endl;
 
     return 0;
 }
