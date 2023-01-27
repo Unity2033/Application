d67d7ac87695e979bdea0b5c28b365f43057a54c
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-01-27 19:46:39 +0900

    (2023.1.27) 클래스의 정적 멤버 변수

--- Application.cpp
+++ Application.cpp
@@ -2,108 +2,115 @@
 
 using namespace std;
 
-#pragma pack(7)
-// 바이트 패딩을 하지 않고 1 byte로 계산해서 사용하는 전치리기입니다.
-class Unit
+class Monster
 {
 public :
-    char grade;
-    short mp;
-
     int health;
-    int attack;
+    static int count;
 
-    Unit()
+    // this 포인터 
+    // 자기 자신을 가리키는 포인터입니다.
+    Monster(int health)
     {
-        cout << "Unit 객체 생성" << endl;
+        // this 포인터는 호출된 객체의 주소를 가리키기 때문에
+        // 상수 포인터입니다.
+        this->health = health;
     }
 
-    void Skill(string skillName)
+    void Tracking()
     {
-        cout << skillName << endl;
+        cout << this << " : 호출했습니다." << endl;
+        cout << "몬스터 추적" << endl;
     }
 
-    ~Unit()
+    ~Monster()
     {
-        cout << "Unit 객체 파괴" << endl;
+        count++;
+        cout << "몬스터를 처치한 수 : " << count << endl;
     }
-
-
 };
+    
+// static 멤버 변수는 모든 객체들이 접근해야 하므로
+// 프로그램 시작부터 끝까지 유지되어야 하기 때문에
+// 전역 범위에서만 정의할 수 있습니다.
+int Monster::count = 0;
 
-// protected 상속
-// 상속받은 클래스가 protected보다 접근 범위가 넓은
-// 접근 지정자(public)를 모두 protected으로 
-// 설정하는 상속입니다.
+// 다중 상속이란?
+// 하나의 하위 클래스가 여러개의 상위 클래스를
+// 상속받는 것입니다.
 
-class Ghost : protected Unit
+class KeyBoard
 {
 public :
-    int defense;
-    Ghost()
-    {
-        defense = 10;
-        health = 100;
-        attack = 25;
+    bool power;
 
-        // Skill("Lock Down");
-        cout << "Ghost 객체 생성" << endl;
-    }
-
-    ~Ghost()
+    void Typing()
     {
-        cout << "Ghost 객체 파괴" << endl;
+        cout << "키보드 입력" << endl;
     }
 };
 
-// private 상속
-// 상속받은 클래스가 private보다 접근 범위가 넓은
-// 접근 지정자(public, protected)를 모두 private으로 
-// 설정하는 상속입니다.
+class Mouse
+{
+public:
+    bool power;
 
-class Duran : private Ghost
+    void Drag()
+    {
+        cout << "마우스 드래그" << endl;
+    }
+};
+
+class Computer : public KeyBoard, public Mouse
 {
 public :
-    Duran()
+    void Use()
     {
-        health = 300;
-        attack = 30;
+        Mouse::power = true;
 
-        // Skill("Clocking");
-
-        cout << "Duran 객체 생성" << endl;
-    }
-
-    ~Duran()
-    {
-        cout << "Duran 객체 파괴" << endl;
+        Typing();
+        Drag();
     }
 };
+
 
 int main()
 {
-    // 상속이란?
+    // 정적 변수
     /*
-    // 상위 클래스의 속성을 하위 클래스가 사용 할 수 있도록 
-    // 설정해주는 기능입니다.
+    Monster * m1 = new Monster;
+    m1->health = 100;
 
-    //Unit unit;
-    //cout << "unit의 크기 : " << sizeof(unit) << endl;
-    
-    // Ghost ghost;
-    // cout << "ghost의 크기 : " << sizeof(ghost) << endl;
+    delete m1;
 
-    Duran duran;
-    cout << "duran의 크기 : " << sizeof(duran) << endl;
+    Monster* m2 = new Monster;
+    m2->health = 100;
+
+    delete m2;
     */
 
+    // this 포인터
+    /*
+    Monster m1(100);
+    Monster m2(125);
+    Monster m3(175);
 
-#pragma region 클래스의 바이트 패딩
+    m1.Tracking();
+    m2.Tracking();
+    m3.Tracking();
+    */
 
-    Unit unit;
+    // 다중 상속
+    Computer computer;
+    computer.Use();
 
-    cout << "unit의 크기 : " << sizeof(unit) << endl;
-#pragma endregion
+    // 다중 상속은 여러 개의 상위 클래스에 중복되는
+    // 속성이 존재할 수 있기 때문에 범위 지정 
+    // 연산자를 통해서 상위 클래스의 이름을 선언하고
+    // 속성을 사용해야 합니다.
+
+    computer.KeyBoard::power = true;
+
 
 
     return 0;
