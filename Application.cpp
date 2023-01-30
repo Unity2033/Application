9b6db76438d5d2a77921748f56f7817df56cbf6d
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-01-30 19:48:07 +0900

    (2023.1.30) 함수의 오버로딩과 오버라이딩

--- Application.cpp
+++ Application.cpp
@@ -2,117 +2,117 @@
 
 using namespace std;
 
-class Monster
+class Cat
 {
-public :
-    int health;
-    static int count;
-
-    // this 포인터 
-    // 자기 자신을 가리키는 포인터입니다.
-    Monster(int health)
-    {
-        // this 포인터는 호출된 객체의 주소를 가리키기 때문에
-        // 상수 포인터입니다.
-        this->health = health;
-    }
-
-    void Tracking()
-    {
-        cout << this << " : 호출했습니다." << endl;
-        cout << "몬스터 추적" << endl;
-    }
-
-    ~Monster()
-    {
-        count++;
-        cout << "몬스터를 처치한 수 : " << count << endl;
-    }
-};
-    
-// static 멤버 변수는 모든 객체들이 접근해야 하므로
-// 프로그램 시작부터 끝까지 유지되어야 하기 때문에
-// 전역 범위에서만 정의할 수 있습니다.
-int Monster::count = 0;
-
-// 다중 상속이란?
-// 하나의 하위 클래스가 여러개의 상위 클래스를
-// 상속받는 것입니다.
-
-class KeyBoard
-{
-public :
-    bool power;
-
-    void Typing()
-    {
-        cout << "키보드 입력" << endl;
-    }
+public : 
+    int age;
+    char blood;
+    double weight;
 };
 
-class Mouse
+// 함수의 오버로딩
+// 같은 이름의 함수를 매개 변수의 자료형과 매개 변수의 수로
+// 구분하여 여러 개를 선언할 수 있는 기능입니다.
+
+// 2 차원 Vector 함수
+void Vector(float x, float y)
+{
+    cout << "2차원 Vector(float) 함수" << endl;
+}
+
+void Vector(int x, int y)
+{
+    cout << "2차원 Vector(int) 함수" << endl;
+}
+
+// 3 차원 Vector 함수
+void Vector(float x, float y, float z)
+{
+    cout << "3차원 Vector(float) 함수" << endl;
+}
+
+void Vector(int x, int y, int z)
+{
+    cout << "3차원 Vector(int) 함수" << endl;
+}
+
+// 함수의 오버로딩의 경우 함수의 매개 변수에 전달하는
+// 인수의 형태를 보고 호출하므로, 반환형으로 함수의
+// 오버로딩을 생성할 수 없습니다.
+/*
+int Vector(int x, int y)
+{
+    return 0;
+}
+*/
+
+// 함수의 오버라이드
+// 상위 클래스에 있는 함수를 하위 클래스에서 재정의하여
+// 사용하는 기능입니다.
+class Animal
+{
+public :
+    void Sound()
+    {
+        cout << "동물의 울음소리" << endl;
+    }
+};
+
+class Dog : public Animal
 {
 public:
-    bool power;
-
-    void Drag()
+    void Sound()
     {
-        cout << "마우스 드래그" << endl;
+        cout << "강아지의 울음소리" << endl;
     }
+
 };
-
-class Computer : public KeyBoard, public Mouse
-{
-public :
-    void Use()
-    {
-        Mouse::power = true;
-
-        Typing();
-        Drag();
-    }
-};
-
 
 int main()
 {
-    // 정적 변수
+    // 클래스 오프셋 
     /*
-    Monster * m1 = new Monster;
-    m1->health = 100;
+    // 동일한 프로젝트 안에서 처음부터 주어진 요소나 지점까지의 변위차를 
+    // 나타내는 정수형입니다.
+    Cat cat;
 
-    delete m1;
+    cat.age = 5;
+    cat.blood = 'O';
+    cat.weight = 5.75f;
 
-    Monster* m2 = new Monster;
-    m2->health = 100;
+    cout << (int)(&(((Cat*)0)->age)) << endl;
+    cout << (int)(&(((Cat*)0)->blood)) << endl;
+    cout << (int)(&(((Cat*)0)->weight)) << endl;
 
-    delete m2;
+    cout << "cat의 메모리 크기 : " << sizeof(cat) << endl;
     */
 
-    // this 포인터
-    /*
-    Monster m1(100);
-    Monster m2(125);
-    Monster m3(175);
+    // 다형성이란?
+    // 여러 개의 서로 다른 객체가 동일한 기능을 서로 다른
+    // 방법으로 처리할 수 있는 작업입니다.
 
-    m1.Tracking();
-    m2.Tracking();
-    m3.Tracking();
+    // 함수의 오버로딩
+    /*
+    Vector(10, 20);
+    Vector(10, 20, 30);
+
+    Vector(5.95f, 25.5f);
+    Vector(10.75f, 15.35f, 2.5f);
     */
 
-    // 다중 상속
-    Computer computer;
-    computer.Use();
+    // 함수의 오버라이드는 상속 관계에서만 이루어지며,
+    // 하위 클래스에서 함수를 재정의할 때 상위 클래스의
+    // 함수 형태와 일치해야 합니다.
 
-    // 다중 상속은 여러 개의 상위 클래스에 중복되는
-    // 속성이 존재할 수 있기 때문에 범위 지정 
-    // 연산자를 통해서 상위 클래스의 이름을 선언하고
-    // 속성을 사용해야 합니다.
+    Dog * dog = new Dog();
+    dog->Sound();
 
-    computer.KeyBoard::power = true;
+    Animal * animal = new Animal();
+    animal->Sound();
 
+    animal = dog;
 
-
+    animal->Sound();
     return 0;
 }
 
