0091283c97fd22f7ed1ba096ca7a58846ce67464
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-02-08 19:49:27 +0900

    (2023.2.8) 업 캐스팅 & 다운 캐스팅

--- Application.cpp
+++ Application.cpp
@@ -1,98 +1,120 @@
 #include <iostream> // c++ 입출력 헤더 <iostream> 
-#include <map>
-#include <set>
 
 using namespace std;
 
-int main()
+class Person
 {
-    // map
-    /*
-    // key와 value로 데이터를 저장시키는 자료구조입니다.
-    map<string, int> mapData;
-
-    mapData.insert(pair<string, int>("검", 100));
-    mapData.insert(pair<string, int>("갑옷", 200));
-    mapData.insert(pair<string, int>("투구", 300));
-    mapData.insert(pair<string, int>("신발", 400));
-    mapData.insert(pair<string, int>("장갑", 500));
-    mapData.insert(pair<string, int>("각반", 1800));
-
-    // 반복자
-    map<string, int>::iterator iter;
-
-    // begin() : map 데이터의 시작 주소를 반환합니다.
-    // mapData.begin();
-
-    // end() : map 데이터의 마지막 뒤에 있는 주소를 반환합니다.
-
-    // 연관 컨테이너는 KEY 값이 중복될 수 없습니다.
-    // 연관 컨테이너는 VALUE 값은 중복될 수 있습니다.
-
-    // mapData.find("검")
-    // find() : map에 있는 KEY 값이 있는지 없는 지 확인하는 함수입니다.
-    // mapData.find("검")
-    
-    // map에서 데이터를 찾을 때 iterator를 사용하여 
-    // map 내부에 데이터를 전체 탐색하여 있다면 KEY 값이 있는 거고,
-    // map 내부에 데이터가 없다면 없는 형태로 동작합니다.
-    if (mapData.find("검") != mapData.end())
+public:
+    virtual void Talk()
     {
-        cout << "KEY 값 검이 있습니다." << endl;
+        cout << "말하기" << endl;
     }
-    else
+};
+
+class Student : public Person
+{
+public:
+    void Study()
     {
-        cout << "KEY 값이 없습니다.";
-    }
-   
-    for (iter = mapData.begin(); iter != mapData.end(); iter++)
-    {
-        // iter->first : KEY
-        cout << "KEY 값 : " << iter->first << endl;
-        cout << "VALUE 값 : " << iter->second << endl;
+        cout << "학습 중~" << endl;
     }
 
-    cout << "------------------------------------------" << endl;
-
-    map<string, int>::reverse_iterator iter1;
-
-    for (iter1 = mapData.rbegin(); iter1 != mapData.rend(); ++iter1)
+    // final이란?
+    // 특정한 상속 영역까지만 상속하겠다고 제한하는 키워드입니다.
+    void Talk() final
     {
-        cout << "KEY 값 : " << iter1->first << endl;
-        cout << "VALUE 값 : " << iter1->second << endl;
+        cout << "학생이 질문한다." << endl;
+    }
+};
+
+class ModelStudent : public Student 
+{
+public :
+    void HighGrades()
+    {
+
+    }
+
+    /*  
+    void Talk()
+    {
+
     }
     */
+};
 
-    // set
-    set<int> setData;
-
-    setData.insert(10);
-    setData.insert(10);
-    setData.insert(20);
-    setData.insert(30);
-    setData.insert(40);
-    setData.insert(50);
-
-    set<int>::iterator iter;
-
-    for (iter = setData.begin(); iter != setData.end(); iter++)
+class A
+{
+public :
+    virtual void Attack()
     {
-        cout << *iter << endl;
+        cout << "공격" << endl;
     }
 
-    iter = setData.find(50);
-
-    if (iter != setData.end())
+    virtual void Move()
     {
-        cout << "값을 찾았습니다." << endl;
+        cout << "이동" << endl;
     }
-    else
+};
+
+class B : public A
+{
+public :
+    void Attack()
     {
-        cout << "값을 찾지 못했습니다." << endl;
+        cout << "칼 들고 공격" << endl;
     }
 
-    cout << "setData의 사이즈 : " << setData.size() << endl;
+    void Move()
+    {
+        cout << "칼 들고 이동" << endl;
+    }
+};
 
+int main()
+{
+    // 상위 클래스 <- 하위 클래스 
+    // 업캐스팅이란?
+    /*
+    // 하위 클래스의 정보를 담을 수 있는 객체에
+    // 상위 클래스의 자료형을 부여해서 상위클래스처럼 
+    // 사용하는 캐스팅입니다.
+
+    A * aptr;
+
+    B * bptr = new B();
+
+    aptr = bptr;
+
+    aptr->AClass();
+    */
+
+    // 다운캐스팅
+    // 하위 클래스 <- 상위 클래스 
+
+    // int a;
+    // char c;
+
+    // 1 byte       4 byte
+    //   c     =    a;
+
+    int select = 0;
+    cin >> select;
+
+    A * aptr = nullptr;
+
+    switch (select)
+    {
+    case 1:
+        aptr = new B();
+        break;
+
+    // case 2:
+    //    aptr = new C();
+    //    break;
+    }
+
+    aptr->Attack();
 
     return 0;
 }
